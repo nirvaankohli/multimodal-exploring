220c501baa8f745df3428fcfa49c6f66e99d4e63
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-01 18:33:12 -0400

    stuff happened

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,14 @@
 # --------------
 import io, os, time, threading
 from PIL import Image, UnidentifiedImageError
-from flask import Flask, request, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
 import torch
 from transformers import VisionEncoderDecoderModel, AutoTokenizer, AutoImageProcessor
 from dotenv import load_dotenv
+from hmac import compare_digest
+from flask import Flask, request, jsonify, g
+from functools import wraps
+import hashlib
 
 # --------------
 # Config
@@ -16,9 +20,27 @@ load_dotenv()
 
 app = Flask(__name__)
 
-API_KEYS = os.getenv("API_KEYS", "").split(", ")
+API_KEYS = [[i[0], i[1], i[2].split('*')] for i in (k.split("|") for k in os.getenv("API_KEYS", "").split(","))]
 
-print("API_KEYS:", API_KEYS)
+
+KEYS  = {}
+
+for key in range(len(API_KEYS)):
+
+    key = API_KEYS[key]
+    
+    KEYS["key_" + str(key)] = {
+
+        "hash": hashlib.sha256(key[0].encode()).hexdigest(),
+        "owner": key[1],
+        "scopes": key[2]
+
+    }
+
+
+def verify_api_key(raw_header: str):
+    filler = (1)
+
 
 # --------------
 # Auth
@@ -28,7 +50,20 @@ print("API_KEYS:", API_KEYS)
 
 def home():
 
-    return "Hello, Flask is running!"
+    return "Hello, this API is up and running!"
+
+@app.route('/generate-caption', methods=['POST'])
+def generate_caption():
+
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
